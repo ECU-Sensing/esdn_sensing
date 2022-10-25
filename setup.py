48172705d9cee498ca9f68ff5e529a90154cb57f
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-10-25 09:29:34 -0400

    Updated Support for PM25

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'esdn_sensing',         # How you named your package folder (MyLib)
   packages = ['esdn_sensing'],   # Chose the same as "name"
-  version = '0.2.0',      # Start with a small number and increase it with every change you make
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python library to deliver sensor specific support to ESDN infrastructure',   # Give a short description about your library
   author = 'Colby Sawyer',                   # Type in your name

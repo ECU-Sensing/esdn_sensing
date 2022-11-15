311d41de5371ef3394dc8fbf82085640d3eb4ca6
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-11-15 16:04:15 -0500

    Updated Documentation

--- examples/hydros_ex.py
+++ examples/hydros_ex.py
@@ -1,4 +1,9 @@
-from devices.hydros import Hydros
+from func_timeout import func_timeout, FunctionTimedOut
+from esdn_sensing.hydros import Hydros, SensorError
+#//=========================================
+def use_hydros():
+    return Hydros().get_data()
+#//=========================================
 
 #//=========================================
 def get_data():
@@ -7,7 +12,24 @@ def get_data():
     Returns:
         bytearray: bytearray containing encoded data from the logger (intended for LoRa usage)
     """
-    sensor_data = Hydros().get_data()
+    sensor_data = bytearray(2)
     
+    try:
+        # Get data from device with 30 second timeout
+        hydros_data = func_timeout(30, use_hydros)
+    except (FunctionTimedOut, SensorError) as err:
+        exception = 2
+        print(err)
+    except Exception as err:
+        exception = 3
+        print(err)
+
+    print('Exception: \t' + str(exception))
+    sensor_data[0] = (exception >> 8) & 0xff
+    sensor_data[1] = exception & 0xff
+
+    if exception == 0:
+        sensor_data.extend(hydros_data)
+
     return sensor_data
 #//=========================================
